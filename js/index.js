c972c7fd9a699f3adc77e8b0b37a35fc717fcdb9
albertconceicao <albertconceicao2@gmail.com>
2022-03-14 15:29:00 -0300

    Criando estrutura de Json exemplo para utilizar no carrinho

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
+const dados = require('../json/produtos.json');
+
+console.log(dados.grupos.subgrupos[0]);
+
+
+
 const teste = () => {
     event.preventDefault();
     console.log('Entrou');
 };
 
-const formataValor = (valor) => `${valor}.00`;
+let produto = '';
+let valor = 0;
+let quantidade = 0;
+let subtotal = 0;
+let total = 0;
+
+
+const adicionaCarrinho = (produto, valor, quantidade) => {
+    return `${produto}, ${valor}, ${quantidade} o valor total da compra é de R$${valor * quantidade},00`;
+}
+console.log(adicionaCarrinho("Celular", 50.00, 4));
