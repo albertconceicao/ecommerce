19310a0cde7f1daf283e1857c8b40177508f9833
albertconceicao <albertconceicao2@gmail.com>
2022-03-18 18:32:59 -0300

    criando função criaCard

--- js/grupos.js
+++ js/grupos.js
@@ -9,8 +9,8 @@ $.get("json/GRUPOS.json", function(data) {
     const itemGrupo = document.querySelector("#lista-teste");
     let grupos = "";
     const linkGrupo = dataJson.data.forEach((grupo, x) => grupos += grupo.DESCRI);
-    console.log(grupos);
-
+/*     console.log(grupos);
+ */
     const nomeGrupos = `<a href="" class="nav-link" onclick="mostrarLista('1')" >${grupos}</a>`
     /* return `<a href="" class="nav-link" onclick="mostrarLista('1')" >${dataJson.data[0].DESCRI}</a>`; */
 
@@ -30,16 +30,50 @@ $.get("json/PRODUT.json", function (data) {
     return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
   } 
 
+  function criaCard(titulo, codigo, valor) {
+    console.log(titulo);
+    console.log(codigo);
+    console.log(valor);
 
-  let dataProdutoString = JSON.stringify(data);
+    /* const cardBody = querySelector(".card-body"); */
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const cardTitulo = document.createElement("a");
+    cardTitulo.classList.add("card-title");
+    cardTitulo.innerHTML = titulo;
+    
+
+    const cardCodigo = document.createElement("span");
+    cardCodigo.classList.add("codigo");
+    cardCodigo.innerHTML = codigo;
+
+    const cardValor = document.createElement("span");
+    cardValor.classList.add("card-value");
+    cardValor.innerHTML = valor;
+
+
+    card.appendChild(cardTitulo);
+    cardTitulo.appendChild(cardValor);
+    /* card.append(cardBody); */
+    
+    
+  }
+
+  const dataProdutoString = JSON.stringify(data);
   const dataJsonProduto = JSON.parse(dataProdutoString);
   let produtos = "";
   let codigos = "";
   let valores = "";
 
-  const produtosArray = dataJsonProduto.data.forEach(produto => {
+  const produtosArray = dataJsonProduto.data.forEach((produto) => {
     produtos += produto.DESCRICAO;
+    codigos += produto.CODIGO;
+    valores += produto.VENDA;
+    criaCard(produtos, codigos, valores);
   });
+/* 
   $(".card-title").append(produtos);
 
   const codigosArray = dataJsonProduto.data.forEach(codigo => codigos += codigo.CODIGO)
@@ -48,5 +82,5 @@ $.get("json/PRODUT.json", function (data) {
   
   
   $("#codigo").html(`Código: ${codigos}`);
-  $(".card-value").html(valores);
+  $(".card-value").html(valores); */
 })
